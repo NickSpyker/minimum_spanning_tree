84b56b8d9fccb98b05260d46cf2daa3f18786d96
Nicolas Spijkerman <nicospijke@gmail.com>
2025-06-11 04:30:20 +0000

    add comments

--- mst.c
+++ mst.c
@@ -27,12 +27,19 @@ int inMST[MAXV];
 int heapSize = 0;
 HeapNode heap[MAXV];
 
+// Swaps two HeapNode elements in the heap.
+// Parameters:
+//   a - pointer to the first HeapNode
+//   b - pointer to the second HeapNode
 void swap(HeapNode* a, HeapNode* b) {
     HeapNode tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
+// Ensures the min-heap property for the subtree rooted at index i.
+// Parameters:
+//   i - index of the subtree root in the heap array
 void minHeapify(int i) {
     int l = 2 * i + 1;
     int r = 2 * i + 2;
@@ -50,6 +57,9 @@ void minHeapify(int i) {
     }
 }
 
+// Removes and returns the vertex with the minimum key value from the heap.
+// Returns:
+//   The vertex ID with the minimum key value.
 int extractMin() {
     int minV = heap[0].vertex;
     heap[0] = heap[--heapSize];
@@ -57,6 +67,10 @@ int extractMin() {
     return minV;
 }
 
+// Decreases the key value of a given vertex in the heap and restores heap property.
+// Parameters:
+//   v      - vertex ID whose key is to be decreased
+//   newKey - new key value to assign
 void decreaseKey(int v, int newKey) {
     for (int i = 0; i < heapSize; i++) {
         if (heap[i].vertex != v) {
@@ -71,6 +85,10 @@ void decreaseKey(int v, int newKey) {
     }
 }
 
+// Inserts a new vertex with a given key value into the heap.
+// Parameters:
+//   v - vertex ID to insert
+//   k - key value for the vertex
 void insertHeap(int v, int k) {
     int i = heapSize++;
 
@@ -82,6 +100,9 @@ void insertHeap(int v, int k) {
     }
 }
 
+// Executes Prim's algorithm using a min-heap to find the Minimum Spanning Tree.
+// Parameters:
+//   start - starting vertex for Prim's algorithm
 void prim(int start) {
     for (int i = 0; i < num_vertices; i++) {
         key[i] = INT_MAX;
@@ -108,6 +129,12 @@ void prim(int start) {
     }
 }
 
+// Main function: reads input, builds the adjacency matrix, runs Prim's algorithm, and writes output.
+// Parameters:
+//   argc - argument count
+//   argv - argument vector (expects input and output file names)
+// Returns:
+//   0 on success, 1 on error
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s input.txt output.txt\n", argv[0]);
